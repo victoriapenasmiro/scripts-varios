f01d1b31d8a6e7062bd0bde431ba9aa368f069e8
victoriapenasmiro <56310486+victoriapenasmiro@users.noreply.github.com>
2021-06-11 08:39:41 +0200

    Add files via upload
    Script para dividir un archivo csv en subarchivos en función del numero de filas

---
+++ script_split_fichero_csv.ps1
@@ -0,0 +1,19 @@
+﻿$InputFilename = Get-Content 'C:\Users\VictoriaPeñas\Desktop\Prosegur_BR_procesado.csv'
+ $OutputFilenamePattern = 'Prosegur_BR_procesado_'
+ $LineLimit = 5000
+ $line = 0
+ $i = 0
+ $file = 0
+ $start = 0
+ while ($line -le $InputFilename.Length) {
+ if ($i -eq $LineLimit -Or $line -eq $InputFilename.Length) {
+ $file++
+ $Filename = "$OutputFilenamePattern$file.csv"
+ $InputFilename[$start..($line-1)] | Out-File $Filename -Force
+ $start = $line;
+ $i = 0
+ Write-Host "$Filename"
+ }
+ $i++;
+ $line++
+ }
